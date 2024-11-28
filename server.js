6efdf9d2445730e983b051f5933b754c8c703f8e
kanghyeonLee <lkhlkh0823@handong.ac.kr>
2024-11-28 15:52:58 +0900

    v5

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+
+app.get('/api/exchange-rate', async (req, res) => {
+  try {
+    const response = await axios.get(
+      'https://www.koreaexim.go.kr/site/program/financial/exchangeJSON', {
+        params: {
+          authkey: 'nxDAZEku4syG7lztPuMf14ZFiZMOClIL',
+          searchdate: '20241125',
+          data: 'AP01',
+        },
+      }
+    );
+    res.json(response.data); // 데이터를 클라이언트로 전달
+  } catch (error) {
+    console.error('Error fetching data from API:', error.message);
+    res.status(500).json({ error: 'Failed to fetch data from OpenAPI' });
+  }
+});
+
+app.listen(3001, () => {
+  console.log('Server running on http://localhost:3001');
+});

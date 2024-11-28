b1c5879036ce2e6bc4bb23e351b155927fb72a3d
kanghyeonLee <lkhlkh0823@handong.ac.kr>
2024-11-29 00:36:11 +0900

    update code

--- netlify/functions/fetchExchange.js
+++ netlify/functions/fetchExchange.js
@@ -5,7 +5,14 @@ exports.handler = async function (event) {
   console.log("Incoming request to fetchExchange API");
   try {
     const response = await axios.get(
-      "https://67288771270bd0b97555f84b.mockapi.io/api/v1/students"
+      "https://www.koreaexim.go.kr/site/program/financial/exchangeJSON",
+      {
+        params: {
+          authkey: "nxDAZEku4syG7lztPuMf14ZFiZMOClIL",
+          searchdate: "20241125",
+          data: "AP01",
+        },
+      }
     );
     console.log("Response from OpenAPI:", response.data); // 성공 로그
     return {

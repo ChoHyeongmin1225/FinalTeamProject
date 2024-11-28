0a74237071c7b975371d16ef155f0feb0a2b326a
kanghyeonLee <lkhlkh0823@handong.ac.kr>
2024-11-28 21:04:22 +0900

    update code

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 exports.handler = async function (event) {
   try {
     const response = await axios.get(
-      "https://www.koreaexim.go.kr/site/program/financial/exchangeJSON",
+      "http://www.koreaexim.go.kr/site/program/financial/exchangeJSON",
       {
         params: {
           authkey: "nxDAZEku4syG7lztPuMf14ZFiZMOClIL",

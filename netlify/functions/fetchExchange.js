f914a07596a17e80cc1c49dc351db41d2f60b92a
kanghyeonLee <lkhlkh0823@handong.ac.kr>
2024-11-29 01:39:43 +0900

    update code

--- netlify/functions/fetchExchange.js
+++ netlify/functions/fetchExchange.js
@@ -1,9 +1,8 @@
 const axios = require("axios");
+const { parseStringPromise } = require("xml2js");
 
 exports.handler = async function (event) {
   console.log("Incoming request to fetchExchange API");
-
-  
 
   try {
     const response = await axios.get(
@@ -13,16 +12,23 @@ exports.handler = async function (event) {
           display: "10",
           pageno: "1",
         },
-        httpsAgent: agent,
       }
     );
-    console.log("Response from OpenAPI:", response.data); // 성공 로그
+
+    // XML 데이터를 JSON으로 변환
+    const jsonData = await parseStringPromise(response.data, {
+      explicitArray: false, // 단일 항목을 배열로 처리하지 않음
+      trim: true,           // 공백 제거
+    });
+
+    console.log("Parsed JSON data:", jsonData); // 성공 로그
+
     return {
       statusCode: 200,
-      body: JSON.stringify(response.data),
+      body: JSON.stringify(jsonData), // JSON으로 반환
     };
   } catch (error) {
-    console.error("Error fetching OpenAPI data:", error.message); // 에러 로그
+    console.error("Error fetching or parsing OpenAPI data:", error.message); // 에러 로그
     return {
       statusCode: error.response?.status || 500,
       body: JSON.stringify({ error: error.message }),

b02a0911e532d9b4bfc43beb279532d3c6a4b910
kanghyeonLee <lkhlkh0823@handong.ac.kr>
2024-11-28 23:33:22 +0900

    update code

--- netlify/functions/fetchExchange.js
+++ netlify/functions/fetchExchange.js
@@ -1,18 +1,20 @@
-import fetch from 'node-fetch';
+import fetch from 'node-fetch';  // Use import for ES modules
 
-exports.handler = async function (event) {
+export async function handler(event) {
   console.log("Incoming request to fetchExchange API");
+
   try {
     const response = await fetch(
       "https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=nxDAZEku4syG7lztPuMf14ZFiZMOClIL&searchdate=20241125&data=AP01"
     );
-    
+
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
-    
+
     const data = await response.json();
     console.log("Response from OpenAPI:", data); // 성공 로그
+
     return {
       statusCode: 200,
       body: JSON.stringify(data),
@@ -20,7 +22,7 @@ exports.handler = async function (event) {
   } catch (error) {
     console.error("Error fetching OpenAPI data:", error.message); // 에러 로그
     return {
-      statusCode: error.response?.status || 500,
+      statusCode: 500,
       body: JSON.stringify({ error: error.message }),
     };
   }
